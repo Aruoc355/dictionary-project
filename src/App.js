54d5919841054ffec29580fd64ef76b89ee04cbf
Alessia Ruocco <83032347+Aruoc355@users.noreply.github.com>
2021-08-05 15:30:27 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./logo.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/9/94/Reflection_in_a_soap_bubble_edit.jpg"
+          alt="bubble"
+        />
+      </header>
+    </div>
+  );
+}
