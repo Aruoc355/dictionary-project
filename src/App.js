a9046f32e88ae76245fa687b0a26ea8ff620e426
Alessia Ruocco <83032347+Aruoc355@users.noreply.github.com>
2021-08-05 18:16:24 +0000

    created form

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello dictionary App </header>
+        <main className="Main">
+          <Dictionary />
+        </main>
+
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/alessia-ruocco-17a61041/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            ALRU,
+          </a>{" "}
+          Opensourced on{" "}
+          <a
+            href="https://github.com/Aruoc355/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://app.netlify.com/sites/frosty-euclid-ae1756/deploys/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
-      <footer>
-        Coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/alessia-ruocco-17a61041/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          ALRU,
-        </a>{" "}
-        Opensourced on{" "}
-        <a
-          href="https://github.com/Aruoc355/dictionary-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://app.netlify.com/sites/frosty-euclid-ae1756/deploys/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,1 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <div className="Container">
+        <form className="form-control" onSubmit={search}>
+          <div className="row">
+            <div className="col">
+              <input
+                type="search"
+                onChange={handleKeywordChange}
+                placeholder="Tell me the word...🔎"
+                autoFocus={true}
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
