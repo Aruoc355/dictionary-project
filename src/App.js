1066e7c2dfa2ad90619965366a9b4293cfc42529
Alessia Ruocco <83032347+Aruoc355@users.noreply.github.com>
2021-08-05 16:35:34 +0000

    container and footer added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/9/94/Reflection_in_a_soap_bubble_edit.jpg"
-          alt="bubble"
-        />
-      </header>
+      <div className="container">
+        <header className="App-header">Hello dictionary App </header>
+      </div>
+      <footer>
+        Coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/alessia-ruocco-17a61041/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          ALRU,
+        </a>{" "}
+        Opensourced on{" "}
+        <a
+          href="https://github.com/Aruoc355/dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://app.netlify.com/sites/frosty-euclid-ae1756/deploys/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
